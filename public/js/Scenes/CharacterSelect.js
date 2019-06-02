6a86a31debdcfa373e2b269d74ba498432b94001
kurodoll <kurodoll@protonmail.ch>
2019-06-02 20:41:26 +1200

    Begin designing character select screen

--- public/js/Scenes/CharacterSelect.js
+++ public/js/Scenes/CharacterSelect.js
@@ -1,5 +1,16 @@
 class SceneCharacterSelect extends Phaser.Scene {
     constructor() {
         super({ key: 'character_select' });
+
+        // Common text styles.
+        this.default_font = {
+            fontFamily: 'Calibri',
+            fontSize: 13,
+            color: '#FFFFFF'
+        };
+    }
+
+    create() {
+        this.add.text(20, 20, 'a) New character', this.default_font);
     }
 }
